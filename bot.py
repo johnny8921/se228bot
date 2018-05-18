2bd46786d9a2fc305426b57e65c0daa346521b68
Evgeniy Pushkarev <johnny8921@gmail.com>
2018-05-18 21:37:52 +0300

    Init commit for heroku. Many futures not work, because ROSCOMNADZOR ANUS SEBE ZABLOKIRUY

--- bot.py
+++ bot.py
@@ -5,6 +5,16 @@ from telebot import types
 import config
 import telebot
 import os, time
+import subprocess
+import paho.mqtt.subscribe as subscribe
+
+text = u'\U0001F300'
+
+# def readSensors():
+#     p = subprocess.Popen("""sensors""", shell=True, stdout=subprocess.PIPE)
+#     data = p.stdout.readlines()
+#     print(data)
+
 
 apihelper.proxy = {'https':'socks5://503533686:zfC80WpU@185.211.245.142:1090'}
 
@@ -13,26 +23,50 @@ def largeTextFileSender(file, chat_id):
     largeText = open(file, "rb").read()
     splitted_text = util.split_string(largeText, 3000)
     for text in splitted_text:
-        bot.send_message(chat_id, text)
+        bot.send_message(chat_id, text, parse_mode='html')
 
 @bot.message_handler(commands=['start'])
 def welcomeMessage(message):
     largeTextFileSender(file='hello.txt', chat_id=message.chat.id)
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True, row_width=1)
-    itembtn1 = types.KeyboardButton('/Температура')
+    themperature = '/' + u'\U0001F321' + 'Температура'
+    ac = u'\U00002744' + '  AC'
+    itembtn1 = types.KeyboardButton(themperature)
     itembtn2 = types.KeyboardButton('/Таймеры')
-    itembtn3 = types.KeyboardButton('/Кондей')
+    itembtn3 = types.KeyboardButton(ac)
 
     markup.add(itembtn1, itembtn2, itembtn3)
     bot.send_message(message.chat.id, "Выберите действие:", reply_markup=markup)
 
 @bot.message_handler(commands=['Температура'])
 def welcomeMessage(message):
-    text = 'Температура внутри: '
-    text += '+25C'
+    msg = subscribe.simple(config.topicTempInside, hostname=config.iotserver)
+    print(msg.payload.decode("utf-8"))
+    text = u'\U0001F321' + 'Температура в офисе: '
+    text = text + msg.payload.decode("utf-8") + 'C'
+    bot.send_message(message.chat.id, text)
+    msg = subscribe.simple(config.topicTempOutside, hostname=config.iotserver)
+    print(msg.payload.decode("utf-8"))
+    text = u'\U0001F321' + 'Температура на улице: '
+    text = text + msg.payload.decode("utf-8") + 'C'
     bot.send_message(message.chat.id, text)
     print (text)
 
+@bot.message_handler(commands=['Кондей'])
+def acControll(message):
+    text = u'\U0001F300' + ' 123'
+    bot.send_message(message.chat.id, text, parse_mode='html')
+#     msg = subscribe.simple(config.topicTempInside, hostname=config.iotserver)
+#     print(msg.payload.decode("utf-8"))
+#     text = 'Температура в офисе: '
+#     text = text + msg.payload.decode("utf-8") + 'C'
+#     bot.send_message(message.chat.id, text)
+#     msg = subscribe.simple(config.topicTempOutside, hostname=config.iotserver)
+#     print(msg.payload.decode("utf-8"))
+#     text = 'Температура на улице: '
+#     text = text + msg.payload.decode("utf-8") + 'C'
+#     bot.send_message(message.chat.id, text)
+#     print (text)
 @bot.message_handler(commands=['help'])
 def helpMessage(message):
     largeTextFileSender(file='help.txt', chat_id=message.chat.id)
@@ -47,6 +81,14 @@ def find_file_ids(message):
             # А теперь отправим вслед за файлом его file_id
             bot.send_message(message.chat.id, msg.voice.file_id, reply_to_message_id=msg.message_id)
         time.sleep(3)
+
+@bot.message_handler(commands = ['url'])
+def url(message):
+    markup = types.InlineKeyboardMarkup()
+    btn_my_site= types.InlineKeyboardButton(text='Наш сайт', url='https://habrahabr.ru')
+    markup.add(btn_my_site)
+    bot.send_message(message.chat.id, "Нажми на кнопку и перейди на наш сайт.", reply_markup = markup)
+
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
--- config.py
+++ config.py
@@ -1,1 +1,7 @@
 token = '573985435:AAEAVZnNVgHIIE06fKOrV9TH_am2yh-DN6c'
+iotserver = 'iot.eclipse.org'
+iotport = 1883
+iotHostName = "se228/acmodule"
+topicTempInside = iotHostName + "/" + "temperature/inside"
+topicTempOutside = iotHostName + "/" + "temperature/outside"
+timeZone = +3
